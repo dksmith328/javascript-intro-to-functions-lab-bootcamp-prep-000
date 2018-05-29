8dcf531ce15add28762e532e1d5e105fe4df6b4d
Daniel Keith Smith <dksmith328@gmail.com>
2018-05-29 18:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
+  } else if (string === lowercase) {
+    return "I can\'t hear you!"
   }
 }
